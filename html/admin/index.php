315efb386bc86a0cd117592e6e5468e40a0be825
Bryanrobu <1211898@student.roc-nijmegen.nl>
2025-04-14 13:41:35 +0200

    start pagine namen verbeteren

--- html/admin/index.php
+++ html/admin/index.php
@@ -16,7 +16,7 @@ if (!$is_logged_in) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin | Spicy bite</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
